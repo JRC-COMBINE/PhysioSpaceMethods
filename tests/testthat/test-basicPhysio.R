8bc166298caf99927c3d791c6a26bb3048c5a16c
Esfahani <ali.hadizadeh@rwth-aachen.de>
2018-09-21 19:21:17 +0200

    basicPhysio testing reformating

--- tests/testthat/test-basicPhysio.R
+++ tests/testthat/test-basicPhysio.R
@@ -1,20 +1,31 @@
 context("Test basic Physio Score calculations")
 
-SimulatedGeneExpressionData <- matrix(rnorm(n = 100000, mean = 0, sd = 100),ncol = 10, dimnames = list(1:10000,1:10))
-SimulatedReferenceSpace <- matrix(rnorm(n = 100000, mean = 0, sd = 100),ncol = 10, dimnames = list(1:10000,11:20))
-SelfSCORES <- calculatePhysioMap(InputData = SimulatedGeneExpressionData, SimulatedGeneExpressionData)
-SCORES <- calculatePhysioMap(InputData = SimulatedGeneExpressionData, SimulatedReferenceSpace)
+SimulatedGeneExpressionData <- matrix(rnorm(n = 100000, mean = 0, sd = 100),
+                                      ncol = 10, dimnames = list(1:10000,1:10))
+SimulatedReferenceSpace <- matrix(rnorm(n = 100000, mean = 0, sd = 100),
+                                  ncol = 10, dimnames = list(1:10000,11:20))
+SelfSCORES <- calculatePhysioMap(InputData = SimulatedGeneExpressionData,
+                                 SimulatedGeneExpressionData)
+SCORES <- calculatePhysioMap(InputData = SimulatedGeneExpressionData,
+                             SimulatedReferenceSpace)
 SCORESParallel <- calculatePhysioMap(InputData = SimulatedGeneExpressionData,
                                      Space = SimulatedReferenceSpace,
                                      PARALLEL = TRUE)
 
-expectedSelfMax <- -log2(wilcox.test(x = 1:round(nrow(SimulatedGeneExpressionData)*0.05),
-                               y = (round(nrow(SimulatedGeneExpressionData)*0.05)+1):
-                                 (2*round(nrow(SimulatedGeneExpressionData)*0.05)))$p.value)
+expectedSelfMax <-
+    -log2(wilcox.test(
+        x = 1:round(nrow(SimulatedGeneExpressionData) * 0.05),
+        y = (round(nrow(
+            SimulatedGeneExpressionData
+        ) * 0.05) + 1):(2 * round(nrow(
+            SimulatedGeneExpressionData
+        ) * 0.05))
+    )$p.value)
 
 test_that("'calculatePhysioMap' has to have a matrices as input and space",{
   expect_is(SelfSCORES,"matrix")
-  expect_length(SelfSCORES, ncol(SimulatedGeneExpressionData)*ncol(SimulatedReferenceSpace))
+  expect_length(SelfSCORES,
+                ncol(SimulatedGeneExpressionData)*ncol(SimulatedReferenceSpace))
   expect_equal(max(SelfSCORES),expectedSelfMax)
   expect_lt(max(SCORES),expectedSelfMax)
   expect_error(calculatePhysioMap(InputData = SimulatedGeneExpressionData,
